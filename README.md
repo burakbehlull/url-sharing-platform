ff57d20d610ee6622122de943f33de854a0183f9
31000s <burakyabgu@gmail.com>
2023-08-18 23:16:27 +0300

    feat: Create a new React project

--- README.md
+++
@@ -1,2 +0,0 @@
-# url-sharing-platform
-Just like Bento and Linktree, it's a url sharing platform.
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      Merhaba,
+	  ben Burak
+        
+    </div>
+  )
+}
+
+export default App
