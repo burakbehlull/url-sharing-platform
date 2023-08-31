2ef84ab6cd2723c0be72f6084262321e02d2e56f
31000s <burakyabgu@gmail.com>
2023-08-31 23:42:32 +0300

    feat: Completed 'LinkArea' component and its styling

--- src/components/LinkArea.tsx
+++ src/components/LinkArea.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {settings} from '../../settings.json'
 
 export default function LinkArea(){
+
+
+
     return(
         <section id='linkarea'>
-            <h1>Link Area</h1>
+            <div className='spacebox'>
+                {settings.map((item, index)=> <a className='linkbox' href={item.socialUri} key={index}>
+                        <img src={item.imageUri} alt={item.description} />
+                        <h2> {item.socialName} </h2>
+                        <span> {item.address} </span>
+                    </a>
+                )}
+            </div>
         </section>
     )
 }
